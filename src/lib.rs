fe754aabb9bcbfe892be02b883818379c0d263b7
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-10-01 09:06:38 +0200

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,7 @@
-
-use std::ffi::{c_char, CString};
-
 use panduza_platform_core::Plugin;
 
 
-// Create Rc references to the CString objects to ensure they outlive the Plugin structure
-// let name_rc = CString::new(name).unwrap();
-// let version_rc = CString::new(version).unwrap();
 
-// unsafe {
-//     println!("pp {:?}", (*name_rc.as_ptr()) as u8);
-// }
-
-// const fn initialize_name_rc() -> CString {
-//     // Your initialization logic here
-//     CString::new("Hello from DLL").unwrap()
-// }
-
-// static mut NAME_RC: CString = initialize_name_rc();
-
-// ;= CString::new("Hello from DLL").unwrap();
 
 pub extern "C" fn pok() {
     println!("pooook");
