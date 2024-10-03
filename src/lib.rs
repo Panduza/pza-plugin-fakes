5fcb72bc04b1f6b404ff327c3925d4eb13dd620d
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-10-03 14:06:25 +0200

    need a macro to wrap plugin interface

--- src/lib.rs
+++ src/lib.rs
@@ -4,50 +4,100 @@ use std::{
     time::Duration,
 };
 
-use panduza_platform_core::{Factory, Plugin, Producer};
+use panduza_platform_core::{
+    Factory, Plugin, Producer, ProductionOrder, Reactor, ReactorSettings, Runtime,
+};
+use serde_json::Result;
+use serde_json::Value;
 use tokio::time::sleep;
 
-static mut tesst: Option<Arc<Mutex<u32>>> = None;
+///
+/// True when the runtime has been initialized
+///
+static mut RUNTIME_STARTED: bool = false;
+
 static mut FACTORY: Option<Arc<Mutex<Factory>>> = None;
 
-static mut handle: Option<JoinHandle<()>> = None;
+static mut THREAD_HANDLE: Option<JoinHandle<()>> = None;
 
-async fn counter() {
-    loop {
-        println!("I am getting called by Tokio every 2 seconds");
-        // Sleep for 1 second
-        sleep(Duration::from_secs(2)).await;
-    }
-}
+static mut POS: Option<tokio::sync::mpsc::Sender<ProductionOrder>> = None;
 
 #[tokio::main]
-async fn runForever() {
-    let counterTask = tokio::spawn(counter());
-    tokio::try_join!(counterTask).unwrap();
+async fn start_async_runtime(runtime: Runtime) {
+    runtime.task().await.unwrap();
+}
+
+///
+/// Start the runtime
+///
+unsafe fn start_runtime() {
+    //
+    // Already started
+    if RUNTIME_STARTED {
+        return;
+    }
+
+    //
+    //
+    panduza_platform_core::log::init();
+
+    //
+    //
+    let mut factory = Factory::new();
+    factory.add_producers(plugin_producers());
+
+    //
+    let settings = ReactorSettings::new("localhost", 1883, None);
+    let mut reactor = Reactor::new(settings);
+
+    //
+    //
+    let mut runtime = Runtime::new(factory, reactor);
+    runtime.set_plugin("pza-plugin-fakes");
+
+    //
+    //
+    POS = Some(runtime.clone_production_order_sender());
+
+    //
+    // Start thread
+    let __handle: JoinHandle<()> = thread::spawn(move || {
+        start_async_runtime(runtime);
+    });
+    THREAD_HANDLE = Some(__handle);
+
+    //
+    // Set flag
+    RUNTIME_STARTED = true;
 }
 
 pub extern "C" fn pok() {
     println!("pooook");
 
-    let _handle: JoinHandle<()> = thread::spawn(move || {
-        println!("Trying to create new thread for Tokio runtime");
-        runForever();
-    });
-
     unsafe {
-        handle = Some(_handle);
+        start_runtime();
     }
 
     // handle.join().unwrap();
 }
 
-pub extern "C" fn join() {
-    unsafe {
-        handle.take().unwrap().join().unwrap();
-    }
+pub unsafe extern "C" fn join() {
+    THREAD_HANDLE.take().unwrap().join().unwrap();
 }
 
-pub extern "C" fn produce(str_production_order: *const i8) {}
+pub unsafe extern "C" fn produce(str_production_order: *const i8) -> u32 {
+    //
+    // Start runtime if not already
+    start_runtime();
+
+    let po = ProductionOrder::from_c_str_ptr(str_production_order).unwrap();
+    println!("{:?}", po);
+
+    POS.as_mut().unwrap().try_send(po).unwrap();
+
+    // Success
+    0
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn plugin_entry_point() -> Plugin {
