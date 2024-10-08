3f1db10a08ef4921753474e343a7b2a8eebb92d0
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-10-08 16:23:19 +0100

    up

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,6 @@ mod register_map;
 pub fn plugin_producers() -> Vec<Box<dyn Producer>> {
     let mut producers: Vec<Box<dyn Producer>> = vec![];
     producers.push(register_map::producer::RegisterMapProducer::new());
+    producers.push(register_map::producer::RegisterMapProducer2::new());
     return producers;
 }
