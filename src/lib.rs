d4009ab864a570876f827c002ce5ba8273931a96
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-10-09 11:44:58 +0200

    copy dll auto

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,6 @@ mod register_map;
 pub fn plugin_producers() -> Vec<Box<dyn Producer>> {
     let mut producers: Vec<Box<dyn Producer>> = vec![];
     producers.push(register_map::producer::RegisterMapProducer::new());
-    producers.push(register_map::producer::RegisterMapProducer2::new());
+    // producers.push(register_map::producer::RegisterMapProducer2::new());
     return producers;
 }
